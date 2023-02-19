31e4242f6e0a224ba56a469fd9523e446c8f10b5
Carson McMahan <carson.mcmahan@gmail.com>
2023-02-19 00:18:11 -0700

    main.js
    Deleted some unused code.

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,15 +5,3 @@ $(document).ready(function () {
         $('#content').toggleClass('responsive');
     });
 });
-
-const form = document.querySelector("form");
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  const name = document.querySelector("#name").value;
-  const email = document.querySelector("#email").value;
-  const message = document.querySelector("#message").value;
-
-  alert(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
-});
